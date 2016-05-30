74f18af76e8a5109274ca585795b244d193ea616
Aaron Wong <aaronywwong@gmail.com>
2016-05-30 23:27:32 +0930

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+//when you use uppercase for a variable name in java script, you're saying that the variable's value shouldnt be changed
+//'the root defaults to index.html'
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+
+};
+
+app.use(middleware.logger); //causes middleware.logger to be called for every page requested and every route hit
+
+//app.use(middleware.requireAuthentication); //get called for every page requests and every route hit
+
+app.get('/about', middleware.requireAuthentication, function (req, res){
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public')); //static takes the folder you want to expose
+
+
+app.listen(PORT, function (){ //the second argument runs when the server starts
+	console.log('Express server started on port ' + PORT + '!');
+});
